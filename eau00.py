8dbe732692b53a6cfcc473f91c9724b3849ed9da
Jérôme Theytaz <jerome.theytaz@gmail.com>
2023-03-04 22:02:28 -0400

    Added eau00.py

---
+++ eau00.py
@@ -0,0 +1,8 @@
+all_numbers = []
+
+for first_number in range(0, 10):
+    for second_number in range(0, 10):
+        for third_number in range(0, 10):
+            all_numbers.append((first_number, second_number, third_number))
+
+all_numbers
