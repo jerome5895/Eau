9e6eae7fb1ab80488886f2f9dfd03d0d20b9271b
Jérôme Theytaz <jerome.theytaz@gmail.com>
2023-03-09 18:41:18 -0400

    Finished exercice, added functions, eau04.py

--- eau04.py
+++ eau04.py
@@ -1,9 +1,32 @@
 import sys
 
-nbr = int(sys.argv[1])
+# Function to displays next first-number
+def next_nbr_first(nbr):
+    all_first = []
+    for nbr_first in range(nbr, nbr+100):
+        if nbr_first < 2:
+            print("-1")
+            sys.exit()
+        est_premier = True
+        for diviseur in range(2, nbr_first//2+1):
+            if nbr_first % diviseur == 0:
+                est_premier = False
+                break
+        if est_premier:
+            all_first.append(nbr_first)
+    print(all_first[0])
 
-for i in range(nbr+1):
-    if nbr % 2 != 0:
-        print("This number is already odd, the next is .")
+# Function to manage errors
+def try_except():
+    try:
+        nbr = int(sys.argv[1])
+    except ValueError:
+        print("-1")
         sys.exit()
-    else:
+    return nbr
+
+# Convert globales variables
+nbr = try_except()
+
+# Print out next first-number
+next_nbr_first(nbr)
