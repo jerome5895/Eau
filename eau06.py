e14f033fdaf42becba9845766e64ecf66a095be7
Jérôme Theytaz <jerome.theytaz@gmail.com>
2023-03-09 23:19:35 -0400

    Added eau06.py

---
+++ eau06.py
@@ -0,0 +1,30 @@
+import sys
+
+# Function to convert string to "StRiNg-format"
+def UP_lowcase():
+    new_string = ""
+    for n in range(len(string)):
+        if n % 2 == 0:
+            if ord(string[n]) >= 97:
+                new_string += chr(ord(string[n]) - 32)
+            else:
+                new_string += string[n]
+        else:
+            new_string += string[n]
+    return new_string
+
+# Function to manage ValueErrors
+def try_except():
+    try:
+        string = sys.argv[1]
+    except IndexError:
+        print("Invalid input. No string provided.")
+        sys.exit()
+    return string
+
+# Convert globales variables
+string = try_except()
+Good_string = UP_lowcase() 
+
+# Print out input in "StRiNg-format"
+print(Good_string)
